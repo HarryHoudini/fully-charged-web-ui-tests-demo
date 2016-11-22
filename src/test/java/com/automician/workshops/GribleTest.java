0ae9fc64dc482878904d1503cc4ba7d391c701a1
Julia Iluhina <julia.v.iluhina@gmail.com>
2016-11-22 15:01:43 +0200

    E2E test changed

--- src/test/java/com/automician/workshops/GribleTest.java
+++ src/test/java/com/automician/workshops/GribleTest.java
@@ -120,6 +120,9 @@ public class GribleTest {
         storage.column(0).setName("user");
         storage.addColumnAfter(0, "password");
 
+        EXPECT("Column names are changed");
+        storage.shouldHaveColumnHeaders("user", "password");
+
         /* + more "obvious" and friendly for "newcomer"
          * - needs additional DataStorages#table method in context of implementation
          *   - and then also additional TestTable#table method
@@ -135,131 +138,131 @@ public class GribleTest {
          * - "newcomer" need to learn more "widgets" to start using such approach
          */
 
-        THEN("Fill existing table row with data");
-        storage.row(0).fill("vasya", "pupkin1234");
-        storage.shouldHaveRows(
-                asList("vasya", "pupkin1234")
-        );
-
-        AND("Add one more row with data");
-        storage.addRowAfter(0).fill("masha", "4321ahsam");
-        storage.shouldHaveRows(
-                asList("vasya", "pupkin1234"),
-                asList("masha", "4321ahsam")
-        );
-
-        THEN("Save changes");
-        dataStorages.save();
-
-        /* + more "obvious" and friendly for "newcomer"
-         * - needs additional DataStorages#save method in context of implementation
-
-         * ~
-        new ManageButtons().save();
-         * + KISS
-         * - "newcomer" need to learn more "widgets" to start using such approach
-         */
-
-        THEN("Open product test tables");
-        product.open();
-        TestTables testTables =
-                product.openTestTables();
-
-        AND("Add new test table under new category");
-        testTables.addCategory("Users");
-        testTables.addTestTable("Users", "LoginValidation");
-
-        EXPECT("Test table with one column 'editme' and one empty row");
-        Table table =
-                testTables.table();  // or: dataStorages.storage();
-
-        storage.shouldHaveColumnHeaders("editme");
-        storage.shouldHaveRows(
-                asList("")
-        );
-
-        THEN("Configure first column to be connected to data storage prepared above");
-
-        ContextMenu menu;
-
-        menu = table.column(0).menu();
-        menu.open();
-        /* + more readable
-         * ~
-        menu = table.column(0).menu().open();
-         * + more laconic
-         * - less readable because too long
-         */
-
-        menu.inputFor("Column name").setValue("user");
-        menu.inputFor("Data storage").click();
-        menu.selectFor("Data storage").selectOption("Credentials");
-
-        /* ~
-        new Form(menu.element()).fill(
-                new InputValue("Column name", "user"),
-                new SelectValue("Data storage", "Credentials")
-        );
-         */
-
-        /* <
-        menu.form().fill(
-                ValueFor.input("Column name", "user"),
-                ValueFor.select("DataStorage", "Credentials")
-        );
-        */
-
-        menu.select("Save");
-
-        THEN("Connect first cell of this column to first row from data storage");
-        table.row(0).cell(0).fill("1");
-        table.row(0).cell(0).hover();
-
-        EXPECT("Connection works by showing connected data in a cell tooltip (on hover)");
-        table.toolTip().shouldHaveKeyRowCells("", "user", "password");
-        table.toolTip().shouldHaveValueRowCells("1", "vasya", "pupkin1234");
-
-        /* ~
-        new TableToolTip().shouldHaveKeyRowCells("", "user", "password");
-        new TableToolTip().shouldHaveValueRowCells("1", "vasya", "pupkin1234");
-         */
-
-        THEN("Adds one more column for data and fill its cell");
-        table.addColumnAfter(0, "login valid?");
-
-        table.row(0).cell(1).fill("true");
-
-        table.shouldHaveRows(
-                asList("1", "true")
-        );
-
-        THEN("Adds one more row with both connected data storage data and new data");
-        menu = table.row(0).cell(0).menu();
-        menu.open();
-        menu.select("Insert row below");
-        table.row(1).fill("2", "false");
-
-        table.shouldHaveRows(
-                asList("1", "true"),
-                asList("2", "false")
-        );
-
-        table.row(1).cell(0).hover();
-        table.toolTip().shouldHaveKeyRowCells("", "user", "password");
-        table.toolTip().shouldHaveValueRowCells("2", "masha", "4321ahsam");
-
-        THEN("Save changes");
-        testTables.save();
-
-        /* ~
-        new ManageButtons().save();
-         */
-
-        refresh();
-
-        table.shouldHaveRows(
-                asList("1", "true"),
-                asList("2", "false")
-        );
+//        THEN("Fill existing table row with data");
+//        storage.row(0).fill("vasya", "pupkin1234");
+//        storage.shouldHaveRows(
+//                asList("vasya", "pupkin1234")
+//        );
+//
+//        AND("Add one more row with data");
+//        storage.addRowAfter(0).fill("masha", "4321ahsam");
+//        storage.shouldHaveRows(
+//                asList("vasya", "pupkin1234"),
+//                asList("masha", "4321ahsam")
+//        );
+//
+//        THEN("Save changes");
+//        dataStorages.save();
+//
+//        /* + more "obvious" and friendly for "newcomer"
+//         * - needs additional DataStorages#save method in context of implementation
+//
+//         * ~
+//        new ManageButtons().save();
+//         * + KISS
+//         * - "newcomer" need to learn more "widgets" to start using such approach
+//         */
+//
+//        THEN("Open product test tables");
+//        product.open();
+//        TestTables testTables =
+//                product.openTestTables();
+//
+//        AND("Add new test table under new category");
+//        testTables.addCategory("Users");
+//        testTables.addTestTable("Users", "LoginValidation");
+//
+//        EXPECT("Test table with one column 'editme' and one empty row");
+//        Table table =
+//                testTables.table();  // or: dataStorages.storage();
+//
+//        storage.shouldHaveColumnHeaders("editme");
+//        storage.shouldHaveRows(
+//                asList("")
+//        );
+//
+//        THEN("Configure first column to be connected to data storage prepared above");
+//
+//        ContextMenu menu;
+//
+//        menu = table.column(0).menu();
+//        menu.open();
+//        /* + more readable
+//         * ~
+//        menu = table.column(0).menu().open();
+//         * + more laconic
+//         * - less readable because too long
+//         */
+//
+//        menu.inputFor("Column name").setValue("user");
+//        menu.inputFor("Data storage").click();
+//        menu.selectFor("Data storage").selectOption("Credentials");
+//
+//        /* ~
+//        new Form(menu.element()).fill(
+//                new InputValue("Column name", "user"),
+//                new SelectValue("Data storage", "Credentials")
+//        );
+//         */
+//
+//        /* <
+//        menu.form().fill(
+//                ValueFor.input("Column name", "user"),
+//                ValueFor.select("DataStorage", "Credentials")
+//        );
+//        */
+//
+//        menu.select("Save");
+//
+//        THEN("Connect first cell of this column to first row from data storage");
+//        table.row(0).cell(0).fill("1");
+//        table.row(0).cell(0).hover();
+//
+//        EXPECT("Connection works by showing connected data in a cell tooltip (on hover)");
+//        table.toolTip().shouldHaveKeyRowCells("", "user", "password");
+//        table.toolTip().shouldHaveValueRowCells("1", "vasya", "pupkin1234");
+//
+//        /* ~
+//        new TableToolTip().shouldHaveKeyRowCells("", "user", "password");
+//        new TableToolTip().shouldHaveValueRowCells("1", "vasya", "pupkin1234");
+//         */
+//
+//        THEN("Adds one more column for data and fill its cell");
+//        table.addColumnAfter(0, "login valid?");
+//
+//        table.row(0).cell(1).fill("true");
+//
+//        table.shouldHaveRows(
+//                asList("1", "true")
+//        );
+//
+//        THEN("Adds one more row with both connected data storage data and new data");
+//        menu = table.row(0).cell(0).menu();
+//        menu.open();
+//        menu.select("Insert row below");
+//        table.row(1).fill("2", "false");
+//
+//        table.shouldHaveRows(
+//                asList("1", "true"),
+//                asList("2", "false")
+//        );
+//
+//        table.row(1).cell(0).hover();
+//        table.toolTip().shouldHaveKeyRowCells("", "user", "password");
+//        table.toolTip().shouldHaveValueRowCells("2", "masha", "4321ahsam");
+//
+//        THEN("Save changes");
+//        testTables.save();
+//
+//        /* ~
+//        new ManageButtons().save();
+//         */
+//
+//        refresh();
+//
+//        table.shouldHaveRows(
+//                asList("1", "true"),
+//                asList("2", "false")
+//        );
     }
 }
